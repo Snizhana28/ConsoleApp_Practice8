a348bddf79ea04d71b65c124ec4cbd6753f9cac8
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-09-02 15:32:21 +0300

    task2

---
+++ Domain/Apartment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice8.Domain
+{
+    public class Apartment
+    {
+        /*Створіть клас «Будинок», в якому має міститися інформація про квартири в цьому будинку. Створіть клас 
+        «Квартира» з інформацією про мешканців квартир. Реалізуйте підтримку ітератора для класів «Будинок» і «Квартира». Протестуйте можливість використання foreach для 
+        класів «Будинок» і «Квартира»*/
+        public Dictionary<string, string> Persons { get; } = new Dictionary<string, string>();
+
+        public Apartment()
+        {
+        }
+
+
+
+        public void AddPerson(string name, string surname)
+        {
+            Persons.Add(name, surname);
+        }
+        public void ShowPersons()
+        {
+            foreach (var person in Persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+        public override string ToString()
+        {
+            return $"Apartment with {Persons.Count} persons";
+        }
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return Persons.GetEnumerator();
+        }
+    }
+}
---
+++ Domain/House.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice8.Domain
+{
+    public class House
+    {
+        /*Створіть клас «Будинок», в якому має міститися інформація про квартири в цьому будинку. Створіть клас 
+        «Квартира» з інформацією про мешканців квартир. Реалізуйте підтримку ітератора для класів «Будинок» і «Квартира». Протестуйте можливість використання foreach для 
+        класів «Будинок» і «Квартира»*/
+        public List<Apartment> Apartments { get; set; }
+        public House()
+        {
+            Apartments = new List<Apartment>();
+        }
+        /*public void AddApartment(Apartment apartment)
+        {
+            Apartments.Add(apartment);
+        }
+        public void RemoveApartment(Apartment apartment)
+        {
+            Apartments.Remove(apartment);
+        }*/
+        public void ShowApartments()
+        {
+            foreach (var apartment in Apartments)
+            {
+                Console.WriteLine(apartment);
+            }
+        }
+        public override string ToString()
+        {
+            return $"House with {Apartments.Count} apartments";
+        }
+        public IEnumerator<Apartment> GetEnumerator()
+        {
+            return Apartments.GetEnumerator();
+        }
+    }
+}
