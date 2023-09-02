0cbed568e32ca65376cfb308fb5acb94820b21e1
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-09-02 15:09:59 +0300

    done task1

---
+++ Domain/Alphabet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Practice8.Domain
+{
+    public class Alphabet
+    {
+
+        private List<char> letters;
+        public Alphabet()
+        {
+            letters = new List<char>();
+            for (int i = 0; i < 26; i++)
+            {
+                letters.Add((char)(i + 65));
+            }
+        }
+        public IEnumerator<char> GetEnumerator()
+        {
+            foreach (var item in letters)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ Internal/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp_Practice8.Domain;
+
+namespace ConsoleApp_Practice8.Internal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var alphabet = new Alphabet();
+            foreach (var item in alphabet)
+            {
+                Console.Write($"{item}\t");
+            }  
+        }
+    }
+}
